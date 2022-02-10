733442068bde97173986163690f5967099dc3a99
Pocket7878 <poketo7878@gmail.com>
2022-02-11 01:56:34 +0900

    feature: Support small screen toggle menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,24 @@
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
-import React, { memo } from 'react';
+import React, { memo, useCallback, useState } from 'react';
 import styled from 'styled-components';
 
 export const Header = memo(function Header() {
+  const [menuOpend, setMenuOpened] = useState(false);
+  const handleMenuTogglerClicked = useCallback(() => {
+    setMenuOpened(!menuOpend);
+  }, [menuOpend]);
+
   return (
     <Wrapper>
       <Title>
         <Link href='/'>Pocket7878 Portfolio</Link>
       </Title>
-      <Nav>
+      <MenuToggler onClick={handleMenuTogglerClicked}>
+        <FontAwesomeIcon icon={faBars} height={16} />
+      </MenuToggler>
+      <Nav menuOpened={menuOpend}>
         <MenuItem>
           <Link href='/'>About me</Link>
         </MenuItem>
@@ -23,6 +33,7 @@ export const Header = memo(function Header() {
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   border-bottom: 1px solid #eaeaea;
   position: fixed;
   width: 100%;
@@ -38,14 +49,34 @@ const Title = styled.div`
   font-size: 16px;
   line-height: 16px;
   font-weight: bold;
+  z-index: 998;
 `;
 
-const Nav = styled.nav`
+const MenuToggler = styled.div`
+  height: 48px;
+  width: 48px;
+  padding: 16px;
+  cursor: pointer;
+
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const Nav = styled.nav<{ menuOpened: boolean }>`
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
   background: white;
   height: 100%;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    height: unset;
+    width: 100%;
+    position: relative;
+    display: ${({ menuOpened }) => (menuOpened ? 'flex' : 'none')};
+  }
 `;
 
 const MenuItem = styled.div`
@@ -54,4 +85,16 @@ const MenuItem = styled.div`
   padding: 16px;
   font-size: 16px;
   line-height: 16px;
+  z-index: 997;
+
+  @media only screen and (max-width: 768px) {
+    padding: 0px;
+    a {
+      width: 100%;
+      display: block;
+      height: 48px;
+      padding: 16px;
+      border-bottom: 1px solid #eaeaea;
+    }
+  }
 `;
